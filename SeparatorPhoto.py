0027f870a9a4972557bb32e1a7094e3987826e3d
Alan Victor <alanvictor5447@gmail.com>
2020-06-02 02:02:20 -0300

    code from looking for all the link from pathOrigin specific

--- SeparatorPhoto.py
+++ SeparatorPhoto.py
@@ -1,1 +1,15 @@
+import os
 import cv2
+
+pathOrigin = '/Users/alanvictor/Desktop/FotosInstaIBNA'
+
+
+files = []
+# r=root, d=directories, f = files
+for r, d, f in os.walk(pathOrigin):
+    for file in f:
+        if '.jpg' in file:
+            files.append(os.path.join(r, file))
+
+for f in files:
+    print(f)
